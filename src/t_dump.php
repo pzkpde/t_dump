6cca7ec7b21a9069ea1b47a900929abdaf64b011
Mikhail Volkov <volkov.m@jilfond.ru>
2019-09-12 16:41:14 +0700

    Refactoring

--- src/t_dump.php
+++ src/t_dump.php
@@ -1,36 +1,15 @@
 <?php
 
-if (function_exists('t_dump') === false) {
-    function t_dump($text) {
-
-        $use_config = function_exists('config');
-
-        $display_errors = $use_config ? config('plugins.t_dump.display_errors') : getenv('T_DUMP_DISPLAY_ERRORS');
+if (function_exists('__t_dump') === false) {
+    function __t_dump($text, $chat_id, $token) {
 
         try {
-
-            $chat_id = $use_config ? config('plugins.t_dump.chat_id') : getenv('T_DUMP_CHAT_ID');
-            $token = $use_config ? config('plugins.t_dump.token') : getenv('T_DUMP_TOKEN');
-
-            if (strlen($chat_id) > 0 === false) {
-                if ($display_errors) {
-                    throw new \Exception('Chat ID not specified');
-                }
-                return false;
-            }
-
-            if (strlen($token) > 0 === false) {
-                if ($display_errors) {
-                    throw new \Exception('Token not specified');
-                }
-                return false;
-            }
 
             if (is_array($text) or is_object($text)) {
                 $text = '<pre>' . var_export($text, true) . '</pre>';
             }
 
-            $url = 'https://api.telegram.org/bot' . $token . '/sendMessage?chat_id=' . $chat_id . '&parse_mode=html';
+            $url = 'https://api.telegram.org/bot' . $token . '/sendMessage?chat_id=' . $chat_id;
 
             $ch = curl_init();
 
@@ -41,6 +20,7 @@ if (function_exists('t_dump') === false) {
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_POST => true,
                 CURLOPT_POSTFIELDS => [
+                    'parse_mode' => 'html',
                     'text' => $text,
                 ],
             ]);
@@ -48,22 +28,40 @@ if (function_exists('t_dump') === false) {
             curl_exec($ch);
 
             if (curl_errno($ch)) {
-                if ($display_errors) {
-                    throw new \Exception(curl_error($ch));
-                }
                 return false;
             }
 
             curl_close($ch);
 
         } catch (\Exception $e) {
-            if ($display_errors) {
-                throw new \Exception($e->getMessage(), $e->getCode());
-            }
             return false;
         }
 
         return true;
+    }
+}
+
+if (function_exists('t_dump') === false) {
+    function t_dump($text) {
+
+        $use_config = function_exists('config');
+
+        $chat_id = $use_config ? config('plugins.t_dump.chat_id') : getenv('T_DUMP_CHAT_ID');
+        $token = $use_config ? config('plugins.t_dump.token') : getenv('T_DUMP_TOKEN');
+
+        return __t_dump($text, $chat_id, $token);
+    }
+}
+
+if (function_exists('t_log') === false) {
+    function t_log($text) {
+
+        $use_config = function_exists('config');
+
+        $chat_id = $use_config ? config('plugins.t_log.chat_id') : getenv('T_LOG_CHAT_ID');
+        $token = $use_config ? config('plugins.t_log.token') : getenv('T_LOG_TOKEN');
+
+        return __t_dump($text, $chat_id, $token);
     }
 }
 
